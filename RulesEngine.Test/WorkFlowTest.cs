c2bec573a3f5d54d73a505e028b15db58110775e
Steve Russell <steve.russell@swintontechnology.com>
2021-08-09 17:39:12 +0100

    Add project files.

---
+++ RulesEngine.Test/WorkFlowTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace RulesEngine.Test
+{
+    class WorkFlowTest
+    {
+        [TestCase("", false)]
+        [TestCase(@"C:\", false)]
+        public void Load_BadPath_ReturnsFalse(string rulesPath, bool result)
+        {
+            Assert.AreEqual(result, WorkFlow.Load(rulesPath));
+        }
+    }
+}
---
+++ RulesEngine/WorkFlow.cs
@@ -0,0 +1,10 @@
+﻿namespace RulesEngine
+{
+    public class WorkFlow
+    {
+        public static bool Load(string empty)
+        {
+            return false;
+        }
+    }
+}
